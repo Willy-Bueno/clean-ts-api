29e23351c9e12ae1de66694b52b058dedecac2a9
Willy Camargo <willybueno090@gmail.com>
2022-07-24 22:35:04 -0300

    fix: message unauthorized bug fix

--- src/presentation/errors/unauthorized-error.ts
+++ src/presentation/errors/unauthorized-error.ts
@@ -1,6 +1,7 @@
 export class UnauthorizedError extends Error {
   constructor (stack?: string) {
-    super('Unauthorized')
+    super()
+    this.message = 'Unauthorized'
     this.stack = stack
   }
 }
