f4ba3711f7458a8ea8da053cf3abfe5dff958b61
Willy Camargo <willybueno090@gmail.com>
2022-07-24 21:54:11 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://teste:8545@cluster0.n4qeg.mongodb.net/?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: 'Uq728ojKpEmOvGOXcOJXeFmV+juAa8xXNu5ePbiLQTg='
 }
