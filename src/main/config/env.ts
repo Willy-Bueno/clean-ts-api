80b9a34354ddd1fa2f08f8a6bde5afcc5a44c984
Willy-Bueno <willybueno090@gmail.com>
2022-07-27 12:29:35 -0300

    refactor: change routes to use filesystem instead of fast-glob

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://willy:OLDjSkVuG6B0S4wl@cluster0.ibvzrru.mongodb.net/?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://willy:fRy9d0QH7pyBoKO4@cluster0.ibvzrru.mongodb.net/?retryWrites=true&w=majority',
   port: process.env.PORT || 5050,
   jwtSecret: 'Uq728ojKpEmOvGOXcOJXeFmV+juAa8xXNu5ePbiLQTg='
 }
