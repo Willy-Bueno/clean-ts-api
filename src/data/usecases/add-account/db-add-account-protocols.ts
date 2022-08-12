07dd0165d0a27462acc482f92b91db538b0ec239
Willy-Bueno <willybueno090@gmail.com>
2022-08-12 20:07:35 -0300

    feat: ensure DbAddAccount call LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
+export * from '@/data/protocols/db/account/load-account-by-email-repository'
 export * from '@/data/protocols/security/criptography/hasher'
 export * from '@/domain/usecases/add-account/add-account'
 export * from '@/domain/models/account'
