2f2ed89ffda38266500800453920727d0874d85d
Willy-Bueno <willybueno090@gmail.com>
2022-08-24 16:10:05 -0300

    chore: add module-alias

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://willy:YyG3V6Cgr1SInL5o@cluster0.ibvzrru.mongodb.net/?retryWrites=true&w=majority',
   port: process.env.PORT || 5050,
   jwtSecret: 'Uq728ojKpEmOvGOXcOJXeFmV+juAa8xXNu5ePbiLQTg='
 }
