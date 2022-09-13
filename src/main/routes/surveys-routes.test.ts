b82293f4d8d7d63ad943aba3f8561caaaf390bb1
Willy-Bueno <willybueno090@gmail.com>
2022-09-13 20:37:14 -0300

    test: ensure add survey route return 204 on success

--- src/main/routes/surveys-routes.test.ts
+++ src/main/routes/surveys-routes.test.ts
@@ -3,6 +3,8 @@ import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/main/config/app'
 import { Collection } from 'mongodb'
 import request from 'supertest'
+import env from '../config/env'
+import jwt from 'jsonwebtoken'
 
 let accountCollection: Collection
 let surveysCollection: Collection
@@ -39,6 +41,29 @@ describe('Surveys Routes', () => {
         .post('/api/surveys')
         .send(makeFakeSurvey())
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with role admin', async () => {
+      const account = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const accessToken = jwt.sign({ id: account.insertedId.toHexString() }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: account.insertedId
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(makeFakeSurvey())
+        .expect(204)
     })
   })
 })
