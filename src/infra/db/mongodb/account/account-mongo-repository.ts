5e0fc42ed9f2addc46edbe0349065a541cb5b38b
Willy-Bueno <willybueno090@gmail.com>
2022-09-13 18:17:28 -0300

    refactor: change strategy findOne by token

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -30,14 +30,7 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
 
   async loadByToken (token: string, role?: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({
-      accessToken: token,
-      $or: [{
-        role
-      }, {
-        role: 'admin'
-      }]
-    })
+    const account = await accountCollection.findOne({ accessToken: token, role })
     return account && MongoHelper.map(account)
   }
 }
